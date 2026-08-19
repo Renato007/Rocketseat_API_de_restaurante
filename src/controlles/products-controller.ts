99d2f8c857565bdd8a731886176dd1bd2a866bf7
Renato Soares <renatosilsoa@gmail.com>
2026-08-19 12:14:17 -0300

    Validando dados do produto

--- src/controlles/products-controller.ts
+++ src/controlles/products-controller.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { AppError } from "@/utils/AppErros";
+import { z } from "zod";
 
 class ProductController {
   async index(request: Request, response: Response, next: NextFunction) {
@@ -8,6 +8,21 @@ class ProductController {
       return response.json({ message: "OK" });
     } catch (error) {
       // Proximo processo de execução passando um erro que podem vir de um processo assícrono.
+      next(error);
+    }
+  }
+
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      //validação dos dados da requisição
+      const bodySchema = z.object({
+        name: z.string().trim().min(6),
+        price: z.number().gt(0, { message: "value must be greater than 0" }),
+      });
+      // ler e passa para o zod para validação, caso não seja validado, o zod lança um erro que é capturado pelo catch e passado para o next(error)
+      const { name, price } = bodySchema.parse(request.body);
+      return response.status(201).json({ name, price });
+    } catch (error) {
       next(error);
     }
   }
--- src/middlewres/error-handling.ts
+++ src/middlewres/error-handling.ts
@@ -13,9 +13,10 @@ export function errorHandling(
   }
 
   if (error instanceof ZodError) {
+    // verificar se o servidor entendeu o pedido
     return response
       .status(400)
-      .json({ message: "validadtion error", issues: error.format() });
+      .json({ message: "validation error", issues: error.format() });
   }
 
   return response.status(500).json({ message: error.message });
