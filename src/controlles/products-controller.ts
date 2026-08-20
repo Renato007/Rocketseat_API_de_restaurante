925d3eb5e651549c5bf69d3b68a184aa7acebd84
Renato Soares <renatosilsoa@gmail.com>
2026-08-20 11:44:49 -0300

    Removendo produtos

--- src/controlles/products-controller.ts
+++ src/controlles/products-controller.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import { AppError } from "@/utils/AppErros";
 import { knex } from "@/database/knex";
 import { z } from "zod";
 
@@ -52,10 +53,44 @@ class ProductController {
       });
 
       const { name, price } = bodySchema.parse(request.body);
+      const product = await knex<ProductRepository>("products")
+        .select()
+        .where({ id })
+        .first();
+      if (!product) {
+        throw new AppError("Product not found");
+      }
 
       await knex<ProductRepository>("products")
         .update({ name, price, updated_at: knex.fn.now() })
         .where({ id });
+
+      return response.json();
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async remove(request: Request, response: Response, next: NextFunction) {
+    try {
+      const id = z
+        .string()
+        .transform((value) => Number(value))
+        .refine((value) => !isNaN(value), { message: "id must be a number" })
+        .parse(request.params.id);
+
+      //para resolver o problema de id número não encontrado
+      const product = await knex<ProductRepository>("products")
+        .select()
+        .where({ id })
+        .first();
+      if (!product) {
+        console.log(Error);
+
+        throw new AppError("Product not found");
+      }
+
+      await knex<ProductRepository>("products").delete().where({ id });
 
       return response.json();
     } catch (error) {
